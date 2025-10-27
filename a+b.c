80b1e83e0c930610c7a180556f5d94fda6490867
Ahmad Fakhrul Bawani <ahmadfakhrulbawani2@gmail.com>
2025-10-27 22:23:02 +0700

    Added 27 Oct 2025

---
+++ a+b.c
@@ -0,0 +1,30 @@
+// the explanation is in the bottom
+#include <stdio.h>
+
+int main(void) {
+    int t;
+    scanf("%d", &t);
+    while(t--) {
+        int a, b;
+        scanf("%d+%d", &a, &b);
+        printf("%d\n", a+b);
+    }
+    return 0;
+}
+
+
+/*
+
+Link: https://codeforces.com/problemset/problem/318/A
+
+Repo: https://github.com/ahmadfakhrulbawani2-arch/CodeForces-Dump-22-to-29-Oct-2025
+
+Sometimes I forgot to change the link. Sorry
+*/
+
+/*
+=== EXPLANATION ===
+
+So easy bro
+
+*/
