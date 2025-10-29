3a914be45589615bde336cff31667ebbd0de5432
Ahmad Fakhrul Bawani <ahmadfakhrulbawani2@gmail.com>
2025-10-29 07:34:39 +0700

    Last commit

---
+++ my-first-sorting-problem.c
@@ -0,0 +1,32 @@
+// the explanation is in the bottom
+#include <stdio.h>
+
+int main(void) {
+    int t;
+    scanf("%d", &t);
+    while(t--) {
+        int a, b;
+        scanf("%d %d", &a, &b);
+        if(a > b) printf("%d %d", b, a);
+        else printf("%d %d", a, b);
+        printf("\n");
+    }
+    return 0;
+}
+
+
+/*
+
+Link: https://codeforces.com/problemset/problem/318/A
+
+Repo: https://github.com/ahmadfakhrulbawani2-arch/CodeForces-Dump-22-to-29-Oct-2025
+
+Sometimes I forgot to change the link. Sorry
+*/
+
+/*
+=== EXPLANATION ===
+
+.
+
+*/
